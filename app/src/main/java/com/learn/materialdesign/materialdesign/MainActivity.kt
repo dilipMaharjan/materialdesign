c226a781dea77e4e63cf9630d9a2eba7c2ffb314
crazdrms <dilipmaharjann@gmail.com>
2018-06-02 07:15:28 -0400

    ripple effect

--- app/src/main/java/com/learn/materialdesign/materialdesign/MainActivity.kt
+++ app/src/main/java/com/learn/materialdesign/materialdesign/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.learn.materialdesign.materialdesign
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun rippleEffects(view: View) {
+        startActivity(Intent(this, RippleReffectTextViews::class.java))
+
     }
 }
---
+++ app/src/main/java/com/learn/materialdesign/materialdesign/RippleReffectTextViews.kt
@@ -0,0 +1,12 @@
+package com.learn.materialdesign.materialdesign
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RippleReffectTextViews : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ripple_reffect_text_views)
+    }
+}
